90ff1e94d627fd6bea90139e42ba9b42084b5be3
John Doe <30752740@qq.com>
2019-11-30 14:59:07 +0800

    更新

---
+++ src/main/scala/com/atguigu/scala0722/day02/controlabs/ControlAbsDemo.scala
@@ -0,0 +1,11 @@
+package com.atguigu.scala0722.day02.controlabs
+
+/**
+  * Author lzc
+  * Date 2019/11/30 14:58
+  */
+object ControlAbsDemo {
+    def main(args: Array[String]): Unit = {
+        
+    }
+}
