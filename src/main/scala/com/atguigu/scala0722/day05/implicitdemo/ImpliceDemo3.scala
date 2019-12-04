58c6d89a7f7c9582c01d968bc5be6a60632fc058
John Doe <30752740@qq.com>
2019-12-04 10:53:25 +0800

    更新

---
+++ src/main/scala/com/atguigu/scala0722/day05/implicitdemo/ImpliceDemo3.scala
@@ -0,0 +1,31 @@
+package com.atguigu.scala0722.day05.implicitdemo
+
+import java.time.LocalDate
+
+
+/**
+  * Author lzc
+  * Date 2019/12/4 10:24
+  */
+object ImpliceDemo3 {
+    
+    
+    def main(args: Array[String]): Unit = {
+        
+    
+        
+    }
+    
+    
+}
+
+
+
+/*
+隐式转换简化我们代码的调用, 增强我们一些已有的类的功能
+1. 隐式转换函数
+    implicit def double2Int(d: Double): Int = d.toInt
+2. 隐式类
+3. 隐式参数和隐式值
+
+ */
---
+++ src/main/scala/com/atguigu/scala0722/day05/implicitdemo/ImpliceDemo4.scala
@@ -0,0 +1,28 @@
+package com.atguigu.scala0722.day05.implicitdemo
+
+
+/**
+  * Author lzc
+  * Date 2019/12/4 10:24
+  */
+object ImpliceDemo4 {
+    
+    
+    def main(args: Array[String]): Unit = {
+        
+    
+        
+    }
+    
+    
+}
+
+
+/*
+隐式转换简化我们代码的调用, 增强我们一些已有的类的功能
+1. 隐式转换函数
+    implicit def double2Int(d: Double): Int = d.toInt
+2. 隐式类
+3. 隐式参数和隐式值
+
+ */
