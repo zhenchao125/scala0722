36cb1fbc33fbc2fa4a3f749514eaab0952216774
John Doe <30752740@qq.com>
2019-12-04 11:35:41 +0800

    更新

--- src/main/scala/com/atguigu/scala0722/day05/implicitdemo/ImpliceDemo4.scala
+++ src/main/scala/com/atguigu/scala0722/day05/implicitdemo/ImpliceDemo4.scala
@@ -9,15 +9,13 @@ object ImpliceDemo4 {
     
     def main(args: Array[String]): Unit = {
         implicit val aaa = 100
-        // 括号有没有加,如果没有括号, 使用隐式值, 如果有括号, 使用默认值
+        
         println(add(10))
         
 //        List(10,20,30).sortBy(x => x)()  // Ordering[Int]
     }
     
-    def add(a: Int)(implicit b: Int = 200) = a + b
-    
-    
+    def add(a: Int)(implicit b: Int) = a + b
 }
 
 
